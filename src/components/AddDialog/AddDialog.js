0097c7c58f58253faf5152918b6ef633550dca3f
nayan <nayanbhakhar7075@gmail.com>
2023-04-26 15:03:29 +0530

    UI and formik done

--- src/components/AddDialog/AddDialog.js
+++ src/components/AddDialog/AddDialog.js
@@ -9,150 +9,276 @@ import {
   DialogTitle,
   FormControl,
   FormControlLabel,
-  InputLabel,
   MenuItem,
-  Select,
   TextField,
+  Typography,
 } from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import moment from "moment";
+import { Formik } from "formik";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import NetBanking from "../../mock/NetBanking.json";
+import UPI from "../../mock/UPI.json";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { AddRecordSchema } from "../../utils/FormikSchema/AddRecordSchema";
 import "./AddDialog.css";
 
 const AddDialog = ({ open, handleClose }) => {
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
 
   return (
     <Dialog
-      maxWidth={"lg"}
-      fullWidth={"lg"}
+      maxWidth="lg"
+      fullWidth={true}
       open={open}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">Add Record</DialogTitle>
-      <DialogContent>
-        <Box className="dialog-container">
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <TextField
-              id="creditor-name"
-              label="Creditor Name"
-              variant="outlined"
-            />
-            <TextField
-              id="debtor-name"
-              label="Debtor Name"
-              variant="outlined"
-            />
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <TextField
-              select
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={age}
-              label="Payment Mode"
-              onChange={handleChange}
-            >
-              {["", "CASH", "UPI", "NET BANKING"].map((item) => (
-                <MenuItem value={item}>
-                  {item === "" ? <em>None</em> : item}
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              select
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={age}
-              label="Payment Application"
-              onChange={handleChange}
-            >
-              {NetBanking.map((item) => (
-                <MenuItem value={item}>
-                  {item.name === "" ? <em>None</em> : item.name}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DatePicker label="Pay Date" />
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <DatePicker label="Pay Back Date" />
-            </LocalizationProvider>
-          </Box>
-          <Box sx={{ display: "flex" }}>
-            <TextField
-              id="principal-mount"
-              label="Principal Amount"
-              variant="outlined"
-            />
-          </Box>
-          <Box sx={{ display: "flex", justifyItems: "space-between" }}>
-            <Box
-              sx={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                justifyContent: "space-between",
-              }}
-            >
-              <TextField
-                id="interest-rate"
-                label="WhatsApp Number"
-                variant="outlined"
-              />
-              <FormControl component="fieldset">
-                <FormControlLabel
-                  value="start"
-                  control={<Checkbox />}
-                  label="Is It WhatsApp Number ?"
-                  labelPlacement="start"
-                />
-              </FormControl>
-            </Box>
-            <Box
-              sx={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-              }}
-            >
-              <TextField
-                id="interest-rate"
-                label="Interest Rate %"
-                variant="outlined"
-              />
-              <FormControl component="fieldset">
-                <FormControlLabel
-                  value="start"
-                  control={<Checkbox />}
-                  label="Interest"
-                  labelPlacement="start"
-                />
-              </FormControl>
-            </Box>
-          </Box>
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} variant="outlined">
-          Cancel
-        </Button>
-        <Button onClick={handleClose} variant="contained">
-          Save
-        </Button>
-      </DialogActions>
+
+      <Formik
+        initialValues={{
+          debtorName: "",
+          creditorName: "",
+          debtorNumber: "",
+          paymentMode: "",
+          payDate: moment(new Date()),
+          paybackDate: moment(new Date()),
+          principalAmount: "",
+          interestRate: 0,
+          paymentApplication: "",
+          isInterest: false,
+          isWhatsapp: false,
+        }}
+        validationSchema={AddRecordSchema}
+        onSubmit={(values) => {
+          try {
+            console.log("values is here---------------------->", values);
+          } catch (error) {
+            console.log("error is here---------------------->", error);
+          }
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          setFieldValue,
+          getFieldProps,
+          errors,
+        }) => {
+          const list =
+            getFieldProps("paymentMode").value === "UPI" ? UPI : NetBanking;
+          return (
+            <form onSubmit={handleSubmit}>
+              <DialogContent>
+                <Box className="dialog-container">
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box className="input-error-block">
+                      <TextField
+                        type="text"
+                        id="creditorName"
+                        name="creditorName"
+                        value={values.creditorName}
+                        onChange={handleChange}
+                        label="Creditor Name"
+                        variant="outlined"
+                      />
+                      <Typography>{errors.creditorName}</Typography>
+                    </Box>
+                    <Box className="input-error-block">
+                      <TextField
+                        type="text"
+                        id="debtorName"
+                        name="debtorName"
+                        value={values.debtorName}
+                        onChange={handleChange}
+                        label="Debtor Name"
+                        variant="outlined"
+                      />
+                      <Typography>{errors.debtorName}</Typography>
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box className="input-error-block">
+                      <TextField
+                        select
+                        id="paymentMode"
+                        name="paymentMode"
+                        value={values.paymentMode}
+                        label="Payment Mode"
+                        onChange={handleChange}
+                      >
+                        {["", "CASH", "UPI", "NET BANKING"].map(
+                          (item, index) => (
+                            <MenuItem value={item} key={index}>
+                              {item === "" ? <em>None</em> : item}
+                            </MenuItem>
+                          )
+                        )}
+                      </TextField>
+                      <Typography>{errors.paymentMode}</Typography>
+                    </Box>
+                    <Box className="input-error-block">
+                      {["UPI", "NET BANKING"].includes(
+                        getFieldProps("paymentMode").value
+                      ) && (
+                        <>
+                          <TextField
+                            select
+                            id="paymentApplication"
+                            name="paymentApplication"
+                            value={values.paymentApplication}
+                            label="Payment Application"
+                            onChange={handleChange}
+                          >
+                            {list.map((item, index) => (
+                              <MenuItem value={item} key={index}>
+                                {item.name}
+                              </MenuItem>
+                            ))}
+                          </TextField>
+                          <Typography>{errors.paymentApplication}</Typography>
+                        </>
+                      )}
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box className="input-error-block">
+                      <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <DatePicker
+                          label="Pay Date"
+                          value={values.payDate}
+                          onChange={(value) =>
+                            setFieldValue(
+                              "payDate",
+                              moment(value).local().toDate()
+                            )
+                          }
+                        />
+                      </LocalizationProvider>
+                      <Typography>{errors.payDate}</Typography>
+                    </Box>
+                    <Box className="input-error-block">
+                      <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <DatePicker
+                          label="Pay Back Date"
+                          value={values.paybackDate}
+                          onChange={(value) =>
+                            setFieldValue(
+                              "paybackDate",
+                              moment(value).local().toDate()
+                            )
+                          }
+                        />
+                      </LocalizationProvider>
+                      <Typography>{errors.paybackDate}</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{ display: "flex" }}>
+                    <Box className="input-error-block" sx={{ width: "50%" }}>
+                      <TextField
+                        id="principalAmount"
+                        name="principalAmount"
+                        type="number"
+                        value={values.principalAmount}
+                        onChange={handleChange}
+                        label="Principal Amount"
+                        variant="outlined"
+                      />
+                      <Typography>{errors.principalAmount}</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{ display: "flex", justifyItems: "space-between" }}>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <TextField
+                        id="debtorNumber"
+                        name="debtorNumber"
+                        type="number"
+                        value={values.debtorNumber}
+                        onChange={handleChange}
+                        label="WhatsApp Number"
+                        variant="outlined"
+                      />
+                      <Typography className="error">
+                        {errors.debtorNumber}
+                      </Typography>
+                      <FormControl component="fieldset">
+                        <FormControlLabel
+                          id="isWhatsapp"
+                          name="isWhatsapp"
+                          value={values.isWhatsapp}
+                          onChange={handleChange}
+                          control={<Checkbox />}
+                          label="Is It WhatsApp Number ?"
+                          labelPlacement="start"
+                        />
+                      </FormControl>
+                    </Box>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      {getFieldProps("isInterest").value === true && (
+                        <>
+                          <TextField
+                            id="interestRate"
+                            name="interestRate"
+                            type="number"
+                            value={values.interestRate}
+                            onChange={handleChange}
+                            label="Interest Rate %"
+                            variant="outlined"
+                          />
+                          <Typography className="error">
+                            {errors.interestRate}
+                          </Typography>
+                        </>
+                      )}
+                      <FormControl component="fieldset">
+                        <FormControlLabel
+                          id="isInterest"
+                          name="isInterest"
+                          value={values.isInterest}
+                          onChange={handleChange}
+                          control={<Checkbox />}
+                          label="Interest"
+                          labelPlacement="start"
+                        />
+                      </FormControl>
+                    </Box>
+                  </Box>
+                </Box>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} variant="outlined">
+                  Cancel
+                </Button>
+                <Button type="submit" variant="contained">
+                  Save
+                </Button>
+              </DialogActions>
+            </form>
+          );
+        }}
+      </Formik>
     </Dialog>
   );
 };
--- src/utils/FormikSchema/AddRecordSchema.js
+++ src/utils/FormikSchema/AddRecordSchema.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import * as Yup from "yup";
 
 const phoneRegExp =
@@ -9,9 +10,34 @@ export const AddRecordSchema = Yup.object().shape({
   debtorNumber: Yup.string()
     .required("Please Enter Mobile Number")
     .matches(phoneRegExp, "Mobile number is not valid"),
-  paymentMode: Yup.object().required("Year value is required."),
-  paybackDate: Yup.string().required("Payback Date is required"),
-  payDate: Yup.string().required("Pay Date is required"),
-  principalAmount: Yup.number().required("Pay Date is required"),
-  interestRate: Yup.number().max(100, "Rate should not be more than 100."),
+  paymentMode: Yup.string().required("Payment mode is required."),
+  payDate: Yup.date()
+    .nullable()
+    .required("Pay Date is required")
+    .typeError("Invalid Date!"),
+  paybackDate: Yup.date()
+    .nullable()
+    .required("Payback Date is required")
+    .typeError("Invalid Date!")
+    .when("payDate", {
+      is: (payDate) => moment(payDate).isValid(),
+      then: (payDate) =>
+        Yup.date().when("payDate", (payDate, schema) =>
+          moment(payDate).isValid() ? schema.min(payDate) : schema.min(payDate)
+        ),
+    }),
+  principalAmount: Yup.number().required("Principal amount is required"),
+  isInterest: Yup.boolean(),
+  isWhatsapp: Yup.boolean(),
+  interestRate: Yup.number().when("isInterest", {
+    is: true,
+    then: () =>
+      Yup.number()
+        .required("Interest Rate is required")
+        .max(100, "Rate should not be more than 100."),
+  }),
+  paymentApplication: Yup.object().when("paymentMode", {
+    is: (paymentMode) => ["UPI", "NET BANKING"].includes(paymentMode),
+    then: () => Yup.object().required("Payment Application is required"),
+  }),
 });
